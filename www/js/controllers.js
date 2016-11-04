7cc8dac5b804a765f8e3ca381c00cb365101800f
Regina Imhoff <regina.imhoff@gmail.com>
2016-11-04 09:45:26 -0500

    working on facebook login not working yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('crapper_keeper.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -41,16 +41,133 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+// FACEBOOK login controller from https://ionicthemes.com/tutorials/about/native-facebook-login-with-ionic-framework
+.controller('WelcomeCtrl', function($scope, $state, $q, UserService, $ionicLoading) {
+  // This is the success callback from the login method
+  var fbLoginSuccess = function(response) {
+    if (!response.authResponse){
+      fbLoginError("Cannot find the authResponse");
+      return;
+    }
+
+    var authResponse = response.authResponse;
+
+    getFacebookProfileInfo(authResponse)
+    .then(function(profileInfo) {
+      // For the purpose of this example I will store user data on local storage
+      UserService.setUser({
+        authResponse: authResponse,
+				userID: profileInfo.id,
+				name: profileInfo.name,
+      });
+      $ionicLoading.hide();
+      $state.go('app.home');
+    }, function(fail){
+      // Fail get profile info
+      console.log('profile info fail', fail);
+    });
+  };
+
+  // This is the fail callback from the login method
+  var fbLoginError = function(error){
+    console.log('fbLoginError', error);
+    $ionicLoading.hide();
+  };
+
+  // This method is to get the user profile info from the facebook api
+  var getFacebookProfileInfo = function (authResponse) {
+    var info = $q.defer();
+
+    facebookConnectPlugin.api('/me?fields=name&access_token=' + authResponse.accessToken, null,
+      function (response) {
+				console.log(response);
+        info.resolve(response);
+      },
+      function (response) {
+				console.log(response);
+        info.reject(response);
+      }
+    );
+    return info.promise;
+  };
+
+  //This method is executed when the user press the "Login with facebook" button
+  $scope.facebookSignIn = function() {
+    facebookConnectPlugin.getLoginStatus(function(success){
+      if(success.status === 'connected'){
+        // The user is logged in and has authenticated your app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed request, and the time the access token
+        // and signed request each expire
+        console.log('getLoginStatus', success.status);
+
+    		// Check if we have our user saved
+    		var user = UserService.getUser('facebook');
+
+    		if(!user.userID){
+					getFacebookProfileInfo(success.authResponse)
+					.then(function(profileInfo) {
+						// For the purpose of this example I will store user data on local storage
+						UserService.setUser({
+							authResponse: success.authResponse,
+							userID: profileInfo.id,
+							name: profileInfo.name,
+						});
+
+						$state.go('app.home');
+					}, function(fail){
+						// Fail get profile info
+						console.log('profile info fail', fail);
+					});
+				}else{
+					$state.go('app.home');
+				}
+      } else {
+        // If (success.status === 'not_authorized') the user is logged in to Facebook,
+				// but has not authenticated your app
+        // Else the person is not logged into Facebook,
+				// so we're not sure if they are logged into this app or not.
+
+				console.log('getLoginStatus', success.status);
+
+				$ionicLoading.show({
+          template: 'Logging in...'
+        });
+
+				// Ask the permissions you need. You can learn more about
+				// FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+        facebookConnectPlugin.login(['public_profile'], fbLoginSuccess, fbLoginError);
+      }
+    });
+  };
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+// FACEBOOK LOG OUT
+.controller('HomeCtrl', function($scope, UserService, $ionicActionSheet, $state, $ionicLoading){
+	$scope.user = UserService.getUser();
+
+	$scope.showLogOutMenu = function() {
+		var hideSheet = $ionicActionSheet.show({
+			destructiveText: 'Logout',
+			titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
+			cancelText: 'Cancel',
+			cancel: function() {},
+			buttonClicked: function(index) {
+				return true;
+			},
+			destructiveButtonClicked: function(){
+				$ionicLoading.show({
+				  template: 'Logging out...'
+				});
+
+        // Facebook logout
+        facebookConnectPlugin.logout(function(){
+          $ionicLoading.hide();
+          $state.go('welcome');
+        },
+        function(fail){
+          $ionicLoading.hide();
+        });
+			}
+		});
+	};
+})
